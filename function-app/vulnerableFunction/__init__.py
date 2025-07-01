f3ce2b1ca29e599f56a72f198f1bbc3a057670e8
Andres Guisado <andresguisado1@gmail.com>
2025-07-01 21:19:17 +0200

    Deployment changes

--- function-app/vulnerableFunction/__init__.py
+++ function-app/vulnerableFunction/__init__.py
@@ -1,7 +1,4 @@
 import azure.functions as func
-import logging
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    name = req.params.get('name', 'world')
-    return func.HttpResponse(f"Hello, {name}! This is the vulnerableFunction.") 
+    return func.HttpResponse("Hello from classic function!", status_code=200)
